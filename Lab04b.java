7dbfdda12f6beef0d44db01162c44fd4f12d5f75
Jiahao Xian <jiahaoxian6@gmail.com>
2022-10-03 22:47:26 -0500

    2

--- Lab04b.java
+++ Lab04b.java
@@ -8,28 +8,33 @@ public class Lab04b {
 
     public static void main(String[] args) {
 
-        
-        
+        int[] array = {100, 53, 67, 24, 73, 91, 12, 83, 29, 43, 51, 67, 62, 51, 632, 532, 743, 562, 452};
+
+        mySort(array);
+        System.out.println("The sorted array: ");
+
+        for (int i : array) {
+            System.out.println(i);
+        }
     }
-    
-    public static void mySort(int [] inputArray) {
-        
+
+    public static void mySort(int[] inputArray) {
+
         int arraySize = inputArray.length;
-        
-        for(int i = 0; i < arraySize - 1; i++) {
-            
-            int index = 1;
-            
-            for(int j = 1; j < arraySize; j++) {
+
+        for (int i = 0; i < arraySize - 1; i++) {
+
+            int index = i;
+
+            for (int j = i + 1; j < arraySize; j++) {
                 if (inputArray[j] < inputArray[index]) {
                     index = j;
                 }
             }
-            
+
             int temp = inputArray[i];
             inputArray[i] = inputArray[index];
             inputArray[index] = temp;
-            
         }
     }
 }
